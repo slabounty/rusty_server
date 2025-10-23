2c03cd78decdd180c534cb3dc48680fe74c838c5
Scott LaBounty <slabounty@Scotts-MacBook-Air.local>
2025-10-23 16:51:38 -0700

    Add defaults to port/root in --help

--- src/cli.rs
+++ src/cli.rs
@@ -3,11 +3,11 @@ use clap::{Parser as ClapParser};
 #[derive(ClapParser, Default)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    /// Sets the root root
+    /// Sets the root root (default 8080)
     #[arg(short, long, value_name = "DIRECTORY")]
     pub root: Option<String>,
 
-    /// Sets the port for the server to use
+    /// Sets the port for the server to use (default static/)
     #[arg(short, long, value_name = "PORT")]
     pub port: Option<u16>,
 }
